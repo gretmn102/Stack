8df48fbc53a88a880efc98839db62dcee36bba16
gretmn102 <erlgreywatson@gmail.com>
2025-12-20 15:39:23 +0200

    init

---
+++ Library.fs
@@ -0,0 +1,13 @@
+﻿namespace StackLibrary
+
+type 'a Stack = 'a list
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+[<RequireQualifiedAccess>]
+module Stack =
+    let empty: _ Stack = []
+    let isEmpty = List.isEmpty empty
+    let push x stack = x::stack : _ Stack
+    let pop stack =
+        match stack with
+        | x::xs -> x, (xs: _ Stack)
+        | [] -> failwithf "Stack is empty!"
